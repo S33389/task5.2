c25cefef5f2ed0f8c47c3157b70f5ac0db63de61
S33389 <s33389@pjwstk.edu.pl>
2025-04-10 15:23:21 +0200

    done

---
+++ untitled/src/Film.java
@@ -0,0 +1,25 @@
+
+public class Film extends MediaBiblioteczne {
+    private String rezyser;
+    private int czasTrwania;
+
+    public Film(String tytul, int rokWydania, String rezyser, int czasTrwania) {
+        super(tytul, rokWydania);
+        this.rezyser = rezyser;
+        this.czasTrwania = czasTrwania;
+    }
+    @Override
+    public void wyswietlInformacje() {
+        super.wyswietlInformacje();
+        System.out.println("Rezyser: " + rezyser + "czas trwania: " + czasTrwania);
+    }
+
+    public void sprawdzCzasTrwania(){
+        if(czasTrwania > 120){
+            System.out.println("film jest długi, czas trwania w minutach: " + czasTrwania);
+        }
+        else{
+            System.out.println("film jest standardowy, czas trwania w minutach: " + czasTrwania);
+        }
+    }
+}
